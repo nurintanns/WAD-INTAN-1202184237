9055be57ba3b9eb20d7086df57e119f22e1f0d71
nurintanns <nurintansari12022000@gmail.com>
2020-10-14 21:10:25 +0700

    init commit

--- MODUL2 INTAN/Booking.php
+++ MODUL2 INTAN/Booking.php
@@ -1,19 +1,18 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE html>
+<html>
+    <head>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <title>Booking</title>
+        <style>
+            img {
+                width: 400px;
+            }
+        </style>
 
-    <title>Booking</title>
-  </head>
-  <body>
-  
-  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    </head>
+    <body> 
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand" href="#"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,100 +22,78 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item ">
-                    <a class="nav-link" href="home.php">Home<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" value ="standard" href="Booking.php" >Booking</a>
+                    <a class="nav-link" href="Booking.php">Booking</a>
                 </li>
             </ul>
         </div>
     </nav>
-    
-    <div class = "container">
-    <div class="row">
-    <div class="col-sm-1"></div>
-    <div class="col-sm-4 py-5">
-        <form action = "mybooking.php" method="post">
-        <fieldset class="form-group">
-        <div class="form-group row">
-            <div class="col-sm-12">
-            <label for="nama">Name</label>
-            <input type="text" class="form-control" placeholder="" name="nama">
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <label class="checkin">Check-in</label>
-            <input  class="form-control" type="date" placeholder="dd/mm/yyyy" name="checkin">
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <label class="durasi">Duration</label>
-            <input type="text" class="form-control" placeholder="" name="durasi">
-            <small id="jumdurasi" class="form-text text-muted"> Day(s)</small>
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <label for="room">Room Type</label>
-            <select class="custom-select" id="validationCustom04" required name ="roomtype">
-                    <option value ="1">Standard</option>
-                    <option value ="2">Superior</option>
-                    <option value ="3">Luxury</option>
-                </select>
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <label for="room">Add services</label>
-            <small id="jumdurasi" class="form-text text-muted"> $20/Service</small>
-            <div class="form-check">
-             <input class="form-check-input" type="checkbox" value="1" id="defaultCheck1" name="roomservice">
-            <label class="form-check-label" for="defaultCheck1">Room Service</label>
-            <br>
-            <input class="form-check-input" type="checkbox" value="1" id="defaultCheck1" name ="breakfast">
-            <label class="form-check-label" for="defaultCheck1">Breakfast</label>
-            </div>
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <label for="notelp">Phone Number</label>
-            <input type="text" class="form-control" placeholder="" name="phonenumber">
-            </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-            <button type="submit" class="btn btn-primary btn-block">Book<a href="mybooking.php"></button></a>
-            </div>
-            </div>
-        </fieldset>
-        </form>
-        </div>
-        <div class="col-sm-2"></div>
-        <div class="col-sm-1 mt-5">
-        <?php
-      error_reporting(error_reporting() & ~E_NOTICE) ; ?>
-        <?php
+    <?php
+    error_reporting(error_reporting() & ~E_NOTICE) ;
+    $submit=$_GET['submit'];
+    if (empty($submit)) {
+      $submit += 1;
+    }
+    ?>
+    <br>
+    <div class="container" style="margin-top: 20px;">
+        <div class="row">
+            <div class="col-sm-6">
+                <form action="mybooking.php" method="POST">
+                  <div class="form-group">
+                    <label >Name</label>
+                    <input type="input" class="form-control" name="nama" >
+                  <div class="form-group">
+                    <label>Check In</label>
+                    <input type="date" class="form-control" name="checkin" >
+                  </div>
+                  <div class="form-group">
+                    <label >Duration</label>
+                    <input type="input" class="form-control" name="durasi">
+                    <small id="durasiHelp" class="form-text text-muted">Days(s)</small>
+                  </div>
 
-                if($_GET['roomtype'] == 'standard'){
-                    echo '<img src = "standard.jpg" width="400px" ';
-                    } else  if ($_GET['roomtype'] == 'superior'){
-                    echo '<img src = "superior.jpg" width="400px">';
-                    }
-                    else  if ($_GET ['roomtype'] == 'luxury') {
-                    echo '<img src = "luxury.jpg" width="400px"';
-                    }
+                  <label class="my-1 mr-2" for="label">Room Type</label>
+                  <select class="custom-select my-1 mr-sm-2"  name="roomtype">
+                    <option value="1" >Standard</option>
+                    <option value="2" >Superior</option>
+                    <option value="3" >Luxury</option>
+                  </select>
+                  <br>
+                  <label class="my-1 mr-2" for="label">Add Service(s)</label><br>
+                  <small id="jumdurasi" class="form-text text-muted"> $20/Service</small>
+
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="1" name="roomservice" >
+                    <label class="form-check-label" >
+                      Room Service
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="1" name="breakfast" >
+                    <label class="form-check-label" >
+                      Breakfast
+                    </label>
+                  </div>
+                  <label for="notelp">Phone Number</label>
+                <input type="text" class="form-control" placeholder="" name="phonenumber">
+                  <br>
+                  <button type="submit" class="btn btn-primary" value="submit" name="submit">Book</button>
+                </form>
+            </div>
+          </div>
+            <div class="col-sm-6">
+                <?php
+                echo "<img src=' $submit.jpg' ";
                 ?>
-        </div>
+            </div>
     </div>
-</div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </body>
+
 </html>
--- MODUL2 INTAN/home.php
+++ MODUL2 INTAN/home.php
@@ -1,20 +1,26 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Home</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-
-    <title>Home</title>
-</head>
-
-<body>
+        <style>
+            }
+            img {
+                width: 400px;
+            }
+            h4 {
+                color :blue;
+                text-align: center;
+            }
+            h5 {
+                color :blue;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body> 
     <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand" href="#"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -25,7 +31,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="home.html">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="Booking.php">Booking</a>
@@ -33,42 +39,38 @@
             </ul>
         </div>
     </nav>
-    <div class="container">
-        <div class="judul">
-            <h4>EAD HOTEL</h4>
-            <h5>Welcome To 5 Star Hotel</h5>
-        </div>
-        <div class="mt-5"></div>
-        <div class ="row">
-        <div class="card-deck">
-            <div class="card1">
-            <div class="card">  
-            <form action="Booking.php" method="get">
-              <img src = "standard.jpg" class="card-img1" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Standard</h5>
-                <h6 class="card-title" style = "color: dodgerblue;">$ 90/Day</h6>
+    <br>
+    <h4>EAD HOTEL</h4>
+    <h5>Welcome To 5 Star Hotel</h5>
+
+    <div class="container" style="margin-top: 20px;">
+        <div class="row">
+            <div class="col-sm-4 1">
+                <div class="card" style="width: 18rem; text-align: center;">
+                    <img src="1.jpg" class="card-img-top" alt="...">
+                    <form action="Booking.php" method="GET">
+                    <div class="card-body">
+                    <h5 class="card-title">Standard</h5>
+                <h6 class="card-title text-center" style = "color: dodgerblue;">$ 90/Day</h6>
                 <br>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item bg-light">Facilities</li>
                     <li class="list-group-item">1 Single Bed</li>
                     <li class="list-group-item">1 Bathroom</li>
                   </ul>
-              </div>
-              <div class="card-footer">
-              <input type="hidden" name="roomtype" value="standard">
-              <button type="submit" class="btn btn-primary">Book Now</button>
-              </form>
-              </div>
+                  <button type="submit" value="1" name="submit" class="btn btn-primary">Book Now</button>
+                        </form>
+                        </p>
+                    </div>
+                  </div>
             </div>
-        </div>
-        <div class="card2">
-            <div class="card"> 
-            <form action="Booking.php" method="get"> 
-              <img src="superior.jpg" class="card-img1" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Superior</h5>
-                <h6 class="card-title" style = "color: dodgerblue;">$ 150/Day</h6>
+            <div class="col-sm-4 2">
+                <div class="card" style="width: 18rem; text-align: center;">
+                    <img src="2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <form action="booking.php" method="GET">
+                      <h5 class="card-title">Superior</h5>
+                <h6 class="card-title text-center" style = "color: dodgerblue;">$ 150/Day</h6>
                 <br>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item bg-light">Facilities</li>
@@ -76,20 +78,18 @@
                     <li class="list-group-item">1 Television and Wi-Fi</li>
                     <li class="list-group-item">1 Bathroom with hot water</li>
                   </ul>
-              </div>
-              <div class="card-footer">
-              <input type="hidden" name="roomtype" value="superior">
-              <button type="submit" class="btn btn-primary">Book Now</button>
-             </form>
-              </div>
+                  <button type="submit" value="2" name="submit" class="btn btn-primary">Book Now</button>
+                        </form>
+                        </p>
+                    </div>
+                  </div>
             </div>
-        </div>
-        <div class="card3">
-            <div class="card">  
-            <form action="Booking.php" method="get">
-              <img src="luxury.jpg" class="card-img1" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Luxury</h5>
+            <div class="col-sm-4 3">
+                <div class="card" style="width: 18rem; text-align: center;">
+                    <img src="3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">     
+                      <form action="Booking.php" method="GET">
+                      <h5 class="card-title">Luxury</h5>
                 <h6 class="card-title" style = "color: dodgerblue;">$ 200/Day</h6>
                 <br>
                 <ul class="list-group list-group-flush">
@@ -100,29 +100,21 @@
                     <li class="list-group-item">1 Television and Wi-Fi</li>
                     <li class="list-group-item">1 Workroom</li>
                   </ul>
-              </div>
-              <div class="card-footer">
-              <input type="hidden" name="roomtype" value="luxury">
-              <button type="submit" class="btn btn-primary">Book Now</button>
-            </form>
-              </div>
+                       <button type="submit" value="3" name="submit" class="btn btn-primary">Book Now</button>
+                        </form>
+                        </p>
+                    </div>
+                  </div>
+                
             </div>
-        </div>
-          </div>
+
         </div>
 
+    </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
-</body>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </body>
 
 </html>
